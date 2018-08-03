deea19469ab98c5663a63b3e6d22492fc3fb2cc6
Lzleaf <280747685@qq.com>
2018-08-03 18:02:37 +0800

    create header and footer template

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="navbar navbar-default topnav">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" @click="toggleNav">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+
+        <a href="/" class="navbar-brand">
+          <span class="title">{{ logo.title }}</span>
+          <img :src="logo.src" :alt="logo.title">
+        </a>
+      </div>
+
+      <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
+        <ul class="nav navbar-nav">
+          <li v-for="(item, index) in navList" :class="{ active: index === activeNavIndex }">
+            <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TheHeader',
+    data() {
+      return {
+        logo: {
+          src: `${this.uploadsUrl}sites/ByvFbNlQYVwhvTyBgLdqitchoacDNznN.jpg`,
+          title: 'VuejsCaff'
+        },
+        navList: ["社区", "头条", "问答", "教程"],
+        activeNavIndex: 0,
+        showCollapsedNav: false
+      }
+    },
+    beforeCreate() {
+      this.uploadsUrl = 'https://vuejscaffcdn.phphub.org/uploads/'
+    },
+    methods: {
+      changeNavIndex(index){
+        this.activeNavIndex = index;
+      },
+      toggleNav() {
+        this.showCollapsedNav = !this.showCollapsedNav
+      }
+    }
+  }
+</script>
+
+<!--scoped：添加此属性，则样式只在当前组件起作用-->
+<style scoped>
+  .title { display: none; }
+  .navbar-default .navbar-nav > .active > a { background: rgba(0,0,0,.03);}
+</style>
